0b68f9089e434ff5602da08802486d6c917a89e4
kin3303 <48729027+kin3303@users.noreply.github.com>
2021-02-09 16:22:27 +0900

    Update app_test.js

--- tests/app_test.js
+++ tests/app_test.js
@@ -1,31 +1,8 @@
-var server   = require('../server'),
-    chai     = require('chai'),
-    chaiHTTP = require('chai-http'),
-    should   = chai.should();
-
-chai.use(chaiHTTP);
-
-reqServer = process.env.HTTP_TEST_SERVER || server
-
-describe('Basic routes tests', function() {
-
-    it('GET to / should return 200', function(done){
-        chai.request(reqServer)
-        .get('/')
-        .end(function(err, res) {
-            res.should.have.status(200);
-            done();
-        })
-
-    })
-
-    it('GET to /pagecount should return 200', function(done){
-        chai.request(reqServer)
-        .get('/pagecount')
-        .end(function(err, res) {
-            res.should.have.status(200);
-            done();
-        })
-
-    })
-})
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
